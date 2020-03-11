ec30eec31765bc2c80836cb4b1baa8154289d6a7
Mayda Aduviri <mayapologies6@gmail.com>
2020-03-10 23:04:34 -0400

    anadiendo escenas

---
+++ main.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+#Para las vidas por defecto 3
+export var vidas = 3
+var offset_vida = 80
+var vida = preload("res://GUI/vida.tscn")
+var lista_vidas = []
+
+# para los ataques de tambor
+export var tambores = 3
+
+var tambor = preload("res://GUI/tambor_ataque.tscn")
+var lista_tambores = []
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	crear_vidas()
+	crear_tambores()
+
+func crear_vidas():
+	for i in vidas:
+		var nueva_vida = vida.instance()
+		get_tree().get_nodes_in_group("gui")[0].add_child(nueva_vida)
+		nueva_vida.global_position = $barra_vidas.global_position
+		nueva_vida.global_position.x += offset_vida * i
+		lista_vidas.append(nueva_vida)
+
+func quitar_vidas():
+	if vidas > 1:
+		vidas -= 1
+		lista_vidas[vidas].queue_free()
+	else:
+		print("moriste")
+
+func crear_tambores():
+	for i in tambores:
+		var nuevo_tambor = tambor.instance()
+		get_tree().get_nodes_in_group("gui")[1].add_child(nuevo_tambor)
+		nuevo_tambor.global_position = $barra_tambor.global_position
+		nuevo_tambor.global_position.x += offset_vida * i
+		lista_tambores.append(nuevo_tambor)
+
+func add_tambor():
+	if tambores < 3:
+		tambores += 1
+		var nuevo_tambor = tambor.instance()
+		get_tree().get_nodes_in_group("gui")[1].add_child(nuevo_tambor)
+		nuevo_tambor.global_position = $barra_tambor.global_position
+		nuevo_tambor.global_position.x += offset_vida * lista_tambores.size()
+		lista_tambores.append(nuevo_tambor)
+		
+func quitar_tambores():
+	if tambores > 0:
+		tambores -= 1
+		lista_tambores[tambores].queue_free()
+		lista_tambores.resize(tambores)
+	else:
+		
+		print("sin tambor")
+		
+		
+func _on_jugador_fuego():
+	$spawn_fireball.crearFuego()
+	quitar_tambores()
